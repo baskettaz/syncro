675e608d2a684e0507541a5e71761be9543bf77e
Vesselin Tsvetanov <vcvetanov@yahoo.com>
2026-06-06 23:13:30 +0200

    added my own ordered_set, but not tested

---
+++ syncro/containers/ordered_set.py
@@ -0,0 +1,111 @@
+from collections.abc import (
+    Iterable,
+    Iterator,
+)
+from typing import (
+    Any,
+    Self,
+    TypeVar,
+)
+
+T = TypeVar("T")
+
+
+def flatten(items, ignore_types=(str, bytes)):
+    # Python Cookbook, David Beazley, 3rd Edition, Recipe 4.14
+    for x in items:
+        if isinstance(x, Iterable) and not isinstance(x, ignore_types):
+            yield from flatten(x)
+        else:
+            yield x
+
+
+class OrderedSet(T):
+    @staticmethod
+    def is_iterable(obj: Any) -> bool:
+        return obj and isinstance(obj, Iterable)
+
+    def __init__(self, iterable: Iterable[T] | None = None):
+        self._data: dict[T, None] = {}
+
+        if self.is_iterable(iterable):
+            self.update(flatten(iterable))
+
+    def add(self, item: T) -> None:
+        self._data[item] = None
+
+    def update(self, iterable: Iterable[T]) -> None:
+        for x in iterable:
+            self.add(x)
+
+    def discard(self, item: T) -> None:
+        self._data.pop(item, None)
+
+    def __contains__(self, item: T) -> bool:
+        return item in self._data
+
+    def __iter__(self) -> Iterator[T]:
+        return iter(self._data)
+
+    def __len__(self) -> int:
+        return len(self._data)
+
+    def __repr__(self) -> str:
+        return f"OrderedSet({list(self._data)})"
+
+    def __getitem__(self, index: int) -> T:
+        keys = list(self._data)
+        return keys[index]
+
+    @classmethod
+    def from_keys(cls, keys: Iterable[T]) -> Self:
+        return cls(keys)
+
+    def __or__(self, other: Iterable[T]) -> Self:
+        """Union: self | other"""
+        # Check here ?
+        return self.update(other)
+
+    def __and__(self, other: Iterable[T]) -> Self:
+        """Intersection: self & other"""
+        # Check here ?
+        return OrderedSet(x for x in self if x in other)
+
+    def __sub__(self, other: Iterable[T]) -> Self:
+        """Difference: self - other"""
+        return OrderedSet(x for x in self if x not in other)
+
+    def __xor__(self, other: Iterable[T]) -> Self:
+        """Symmetric difference: self ^ other"""
+        result = []
+        seen = set()
+
+        # elements in self but not in other
+        for x in self:
+            if x not in other:
+                result.append(x)
+                seen.add(x)
+
+        # elements in other but not in self
+        for x in other:
+            if x not in self._data and x not in seen:
+                result.append(x)
+
+        return OrderedSet(result)
+
+    def __ior__(self, other: Iterable[T]) -> Self:
+        self.update(other)
+        return self
+
+    def __iand__(self, other: Iterable[T]) -> Self:
+        self._data = {x: None for x in self if x in other}
+        return self
+
+    def __isub__(self, other: Iterable[T]) -> Self:
+        self._data = {x: None for x in self if x not in other}
+        return self
+
+    def __ixor__(self, other: Iterable[T]) -> Self:
+        result = self ^ other
+        self._data = {x: None for x in result}
+        return self
